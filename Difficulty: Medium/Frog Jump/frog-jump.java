3949250b76bcdd0fc8f5b62633c686c392d6c232
Mayank Bhatt <131894637+9m09@users.noreply.github.com>
2025-08-17 23:30:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -1,37 +1,20 @@
-import java.util.*;
+// User function Template for Java
 class Solution {
     int minCost(int[] height) {
-        int n = height.length;
-        int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[0] = 0;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        pq.add(new int[]{0, 0}); 
-        
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int u = curr[0];
-            int d = curr[1];
-
-            if (d > dist[u]) continue; 
-            if (u + 1 < n) {
-                int newCost = dist[u] + Math.abs(height[u] - height[u+1]);
-                if (newCost < dist[u+1]) {
-                    dist[u+1] = newCost;
-                    pq.add(new int[]{u+1, newCost});
-                }
-            }
-
-            if (u + 2 < n) {
-                int newCost = dist[u] + Math.abs(height[u] - height[u+2]);
-                if (newCost < dist[u+2]) {
-                    dist[u+2] = newCost;
-                    pq.add(new int[]{u+2, newCost});
-                }
-            }
-        }
-        return dist[n-1];
+        // code here
+        int[] dp = new int[height.length];
+        Arrays.fill(dp,-1);
+        return help(dp,height.length-1,height);
     }
-
+    int help(int[] dp,int n,int[] height){
+        if(n==0)    return 0;
+        // if(n==1)    return 1;
+        if(dp[n]!=-1)   return dp[n];
+        int two = Integer.MAX_VALUE;
+        int one=Math.abs(height[n]-height[n-1])+help(dp,n-1,height);
+        if(n-2>-1)
+        two=Math.abs(height[n]-height[n-2])+help(dp,n-2,height);
+        return  dp[n]=Math.min(one,two);
+    }
+    
 }
